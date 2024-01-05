3860c32933d4b402193623c43d62783c023f8086
SUREN S <surenmanju2004@gmail.com>
2024-01-05 19:10:17 +0530

    Update Login.js

--- src/Components/Forms/Login.js
+++ src/Components/Forms/Login.js
@@ -74,7 +74,7 @@ const Login = () => {
           <section className="z-0 mb-4 flex items-center gap-2 whitespace-nowrap text-6xl md:text-8xl lg:gap-4">
             <FaUniversity />
             <h1 className="font-spectral font-semibold  text-slate-900  dark:text-slate-300 ">
-              C
+              K
               <span className="inline-block h-10 w-10 rounded-full bg-violet-900 dark:bg-violet-600 md:h-14 md:w-14 xl:h-14 xl:w-14"></span>
               llege
             </h1>
